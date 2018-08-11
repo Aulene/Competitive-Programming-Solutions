fd61f5c2ead3e8333f3c0b862e6c9309199d6fec
Aulene De <aussylegendflame@gmail.com>
2018-08-11 11:52:34 +0530

    usaco

---
+++ USACO/TTWO.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define int long long int
+#define mod 1000000007
+#define p push
+#define pb push_back
+#define mp make_pair
+#define f first
+#define s second
+
+signed main()
+	{
+		ios_base::sync_with_stdio(false);
+		cin.tie(NULL);
+
+		
+		return 0;
+	}
