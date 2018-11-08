99fa492c9447b14c34b04ebb8251856c924651f5
Aulene De <ad1306@ls.cs.rutgers.edu>
2018-11-08 17:12:04 -0500

    oh

--- Codeforces/609E.cpp
+++ Codeforces/609E.cpp
@@ -17,11 +17,31 @@ using namespace std;
 #define zp mp(0, 0)
 
 const int N = 200007;
+const int LogN = ceil(log2(N));
 
 vector < vector < pi > > g(N);
-vector <ppi> vs;
+vector <ppi> vs, mst;
 
 int lk[N], sz[N];
+int tin[N], tout[N], up[N][LogN];
+int timer = 1;
+
+void dfs(int idx, int p = 1) {
+	
+	tin[idx] = timer++;
+	
+	cout << idx << " " << p << endl;
+
+	up[idx][0] = p;
+
+	for(int i = 1; i <= LogN; i++) 
+		up[idx][i] = up[up[idx][i - 1]][i - 1];
+
+	for(auto it : g[idx])
+		if(it.f != p) dfs(it.f, idx);
+
+	tout[idx] = timer++;
+}
 
 int find(int idx) {
 	while(idx != lk[idx]) idx = lk[idx];
@@ -49,6 +69,7 @@ int kruskals(int n) {
 
 		if(!same(u, v)) {
 			unite(u, v);
+			mst.pb(it);
 			ans += w;
 		}
 	}
@@ -76,14 +97,30 @@ signed main()
 
 		for(i = 0; i < m; i++) {
 			cin >> u >> v >> w;
-			g[u].pb({v, w});
-			g[v].pb({u, w});
 			vs.pb({{u, v}, w});
 		}
 
 		sort(vs.begin(), vs.end(), cmp);
 
-		cout << kruskals(n) << endl;
+		cout << endl;
+		cout << "MST Size = " << kruskals(n) << endl;
+		for(auto it : mst) cout << it.f.f << " " << it.f.s << endl;
+		cout << endl;
+		
+		for(auto it : mst) {
+			int u = it.f.f, v = it.f.s, w = it.s;
+			g[u].pb({v, w});
+			g[v].pb({u, w});
+		}
 
+		dfs(1);
+
+		for(int i = 1; i <= n; i++) cout << tin[i] << " "; cout << endl;
+		for(int i = 1; i <= n; i++) cout << tout[i] << " "; cout << endl;
+
+		cout << endl;
+		for(int i = 1; i <= n; i++) 
+				{ for(int j = 0; j <= LogN; j++) cout << up[i][j] << " "; cout << endl; }
+		
 		return 0;
 	}
