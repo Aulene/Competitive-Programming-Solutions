76ec3d860e50ef60cd0e4df2f70376e90e381339
Aulene De <aussylegendflame@gmail.com>
2018-11-09 14:33:48 -0500

    imlearninggraphs

--- Codeforces/414B.cpp
+++ Codeforces/414B.cpp
@@ -1,45 +1,46 @@
-#include <iostream>
-#include <fstream>
-#include <cstdio>
-#include <cstring>
-#include <cmath>
-#include <climits>
-#include <algorithm>
-#include <vector>
-#include <map>
-#include <unordered_map>
-#include <queue>
-#include <stack>
-#include <set>
-#include <list>
+#include<iostream>
+#include<fstream>
+#include<cstdio>
+#include<cstring>
+#include<cmath>
+#include<climits>
+#include<algorithm>
+#include<vector>
+#include<map>
+#include<queue>
+#include<stack>
+#include<set>
+#include<list>
 
 using namespace std;
 
-#define int long long int
+#define lli long long int
 #define mod 1000000007
 #define p push
 #define pb push_back
 #define mp make_pair
-#define f first
-#define s second
 
-int dp[2007][2007];
+lli n, k;
+lli dp[2007][2007];
 
-signed main()
+int main()
 	{
-		ios_base::sync_with_stdio(false);
-		cin.tie(NULL);
-
-		int n, k, i, j, K;
+		lli i, j, k, K;
+		lli ans=0;
 
 		cin >> n >> K;
 
-		for(i = 1; i <= n; i++) dp[i][1] = 1;
+		for(i=1; i<=n; i++)
+			dp[1][i]=1;
 
-		for(i = 2; i <= K; i++)
-			for(j = 1; j <= n; j++)
-				for(k = j; k <= n; k += j)
-					dp[i][j] = (dp[i][j] + dp[i - 1][k]) % mod;
+		for(i=2; i<=K; i++)
+			for(j=1; j<=n; j++)
+				for(k=j; k<=n; k+=j)
+					dp[i][j]=(dp[i][j]+dp[i-1][k])%mod;
+					
+		for(i=1; i<=n; i++)
+			ans=(ans+dp[K][i])%mod;
 
+		cout << ans << endl;
 		return 0;
 	}
--- Codeforces/609E.cpp
+++ Codeforces/609E.cpp
@@ -17,30 +17,74 @@ using namespace std;
 #define zp mp(0, 0)
 
 const int N = 200007;
-const int LogN = ceil(log2(N));
+const int LogN = 20;
 
 vector < vector < pi > > g(N);
-vector <ppi> vs, mst;
+vector <ppi> vs, vs1, mst;
 
 int lk[N], sz[N];
-int tin[N], tout[N], up[N][LogN];
+int tin[N], tout[N];
+int up[N][LogN + 7], wt[N][LogN + 7];
 int timer = 1;
 
-void dfs(int idx, int p = 1) {
+void dfs(int idx, int p = 1, int w = 0) {
 	
 	tin[idx] = timer++;
-	
-	cout << idx << " " << p << endl;
-
 	up[idx][0] = p;
+	wt[idx][0] = w;
 
-	for(int i = 1; i <= LogN; i++) 
+	for(int i = 1; i <= LogN; i++) {
 		up[idx][i] = up[up[idx][i - 1]][i - 1];
+		wt[idx][i] = max(wt[idx][i - 1], wt[up[idx][i - 1]][i - 1]);
+	}
 
 	for(auto it : g[idx])
-		if(it.f != p) dfs(it.f, idx);
+		if(it.f != p) dfs(it.f, idx, it.s);
 
 	tout[idx] = timer++;
+}
+
+bool ancestor(int u, int v) {
+	if(tin[u] <= tin[v] && tout[u] >= tout[v]) return true;
+	return false;
+}
+
+int lca(int u, int v) {
+	if(ancestor(u, v)) return u;
+	if(ancestor(v, u)) return v;
+
+	for(int i = LogN; i >= 0; i--)
+		if(!ancestor(up[u][i], v)) u = up[u][i];
+
+	return up[u][0];
+}
+
+int lcawt(int u, int v, int lca) {
+	int answt = 0;
+	int ux = u;
+
+	for(int i = LogN; i >= 0; i--)
+		if(!ancestor(up[u][i], v)) {
+			// cout << u << " " << i << " " << wt[u][i] << endl;
+			answt = max(answt, wt[u][i]);
+			u = up[u][i];
+		}
+	
+	if(up[u][0] == lca)
+		answt = max(answt, wt[u][0]);
+
+	u = ux;
+
+	for(int i = LogN; i >= 0; i--)
+		if(!ancestor(up[v][i], u)) {
+			// cout << v << " " << i << " " << wt[v][i] << endl;
+			answt = max(answt, wt[v][i]);
+			v = up[v][i];
+		}
+
+	if(up[v][0] == lca)
+		answt = max(answt, wt[v][0]);
+	return answt;
 }
 
 int find(int idx) {
@@ -60,11 +104,9 @@ void unite(int a, int b) {
 bool cmp(ppi a, ppi b) { return a.s < b.s; }
 
 int kruskals(int n) {
-
 	int ans = 0;
 
 	for(auto it : vs) {
-
 		int u = it.f.f, v = it.f.s, w = it.s;
 
 		if(!same(u, v)) {
@@ -89,7 +131,7 @@ signed main()
 		// ifstream cin ("input.txt");
 		// ofstream cout ("output.txt");
 
-		int n, m, i, j, u, v, w;
+		int n, m, i, j, u, v, w, ans;
 
 		for(i = 0; i < N; i++) lk[i] = i, sz[i] = 1;
 		
@@ -98,14 +140,17 @@ signed main()
 		for(i = 0; i < m; i++) {
 			cin >> u >> v >> w;
 			vs.pb({{u, v}, w});
+			vs1.pb({{u, v}, w});
 		}
 
 		sort(vs.begin(), vs.end(), cmp);
 
-		cout << endl;
-		cout << "MST Size = " << kruskals(n) << endl;
-		for(auto it : mst) cout << it.f.f << " " << it.f.s << endl;
-		cout << endl;
+		int mstwt = kruskals(n);
+
+		// cout << endl;
+		// cout << "MST Size = " << kruskals(n) << endl;
+		// for(auto it : mst) cout << it.f.f << " " << it.f.s << endl;
+		// cout << endl;
 		
 		for(auto it : mst) {
 			int u = it.f.f, v = it.f.s, w = it.s;
@@ -115,12 +160,24 @@ signed main()
 
 		dfs(1);
 
-		for(int i = 1; i <= n; i++) cout << tin[i] << " "; cout << endl;
-		for(int i = 1; i <= n; i++) cout << tout[i] << " "; cout << endl;
+		// for(int i = 1; i <= n; i++) cout << tin[i] << " "; cout << endl;
+		// for(int i = 1; i <= n; i++) cout << tout[i] << " "; cout << endl;
 
-		cout << endl;
-		for(int i = 1; i <= n; i++) 
-				{ for(int j = 0; j <= LogN; j++) cout << up[i][j] << " "; cout << endl; }
-		
+		// cout << endl;
+		// for(int i = 1; i <= n; i++) 
+		// 		{ for(int j = 0; j <= LogN; j++) cout << up[i][j] << " "; cout << endl; }
+		// cout << endl;
+
+		// cout << endl;
+		// for(int i = 1; i <= n; i++) 
+		// 		{ for(int j = 0; j <= LogN; j++) cout << wt[i][j] << " "; cout << endl; }
+		// cout << endl;
+			
+		for(auto it : vs1) {
+			u = it.f.f, v = it.f.s;
+			ans = mstwt - lcawt(u, v, lca(u, v)) + it.s;
+			cout << ans << endl;
+		}
+
 		return 0;
 	}
