3abedc1bb3c74ccd4d21581a103d42f2013b8600
Aulene De <aussylegendflame@gmail.com>
2018-11-11 14:08:21 -0500

    wow

--- Codeforces/342E.cpp
+++ Codeforces/342E.cpp
@@ -1,86 +1,161 @@
-#include<iostream>
-#include<fstream>
-#include<cstdio>
-#include<cstring>
-#include<cmath>
-#include<climits>
-#include<algorithm>
-#include<vector>
-#include<map>
-#include<queue>
-#include<stack>
-#include<set>
-#include<list>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-#define lli long long int
+#define endl '\n'
+#define int long long int
 #define mod 1000000007
 #define p push
 #define pb push_back
 #define mp make_pair
+#define f first
+#define s second
+#define vi vector <int> 
+#define vvi vector < vector <int> > 
+#define pi pair <int, int> 
+#define ppi pair < pair <int, int>, int> 
+#define mid (start + end) / 2
+#define zp mp(0, 0)
 
-vector<int> g[100007], centroidTree[100007];
-int subtrees[100007], decompositions[100007];
-bool vis[100007];
-int centroid, n;
+const int N = 100007;
 
-int findCentroid(int u)
+vvi g(N);
+
+int d[N], tin[N], tout[N];
+int lazy[4 * N];
+int t = 1;
+
+void dfs(int idx, int p = 0) {
+	d[idx] = d[p] + 1;
+	tin[idx] = t++;
+	for(auto it : g[idx])
+		if(it != p) dfs(it, idx);
+	tout[idx] = t++;
+}
+
+struct RangeSTreeLazy
 {
-	subtrees[u] = 1;
-	vis[u] = 1;
-	int left = 0;
+	pi val;
+	RangeSTreeLazy *l, *r;
 
-	for(vector <int> :: iterator it = g[u].begin(); it != g[u].end(); it++)
-		if(!vis[*it])
-			{
-				findCentroid(*it);
-				subtrees[u] += subtrees[*it];
-				left = max(left, subtrees[*it]);
+	pi merge(pi a, pi b) { 
+		pi ans;
+		ans.f = max(a.f, b.f);
+		ans.s = min(a.s, b.s);
+		return ans;
+	}
+
+	RangeSTreeLazy *build(int start, int end)
+		{
+			if(start == end) val = mp(d[1], N);
+			else
+				{
+					l = new RangeSTreeLazy, r = new RangeSTreeLazy;
+					l = l -> build(start, mid), r = r -> build(mid + 1, end);
+					val = merge(l -> val, r -> val);
+				}
+			return this;
+		}
+
+	RangeSTreeLazy *update(int lazy[], int start, int end, int a, int b, int v, int level = 1)	
+		{
+			if(lazy[level] != 0)
+				{
+					val = merge(val, lazy[level]);
+
+					if(start != end)
+						{
+							lazy[2 * level] = lazy[level];
+							lazy[2 * level + 1] = lazy[level];
+						}
+
+					lazy[level] = 0;
+				}
+
+			if(start > b || end < a)
+				return this;
+
+			if(start >= a && end <= b) {
+				val = v;
+
+				if(start != end) {
+					lazy[2 * level] = v;
+					lazy[2 * level + 1] = v;
+				}			
+
+				return this;
 			}
 
-	left = max(left, n - subtrees[u]);
+	// 		l = l -> update(lazy, start, mid, a, b, v, 2 * level);
+	// 		r = r -> update(lazy, mid + 1, end, a, b, v, 2 * level + 1);
+	// 		val = merge(l -> val, r -> val);
+	// 		return this;
+	// 	}
 
-	if(left <= n/2)
-		centroid = u;
+	// int query(int lazy[], int start, int end, int a, int b, int level = 1)
+	// 	{
+	// 		if(lazy[level] != 0)
+	// 			{
+	// 				val = lazy[level];
 
-	return 0;
-}
+	// 				if(start != end)
+	// 					{
+	// 						lazy[2 * level] = lazy[level];
+	// 						lazy[2 * level + 1] = lazy[level];
+	// 					}
 
-int decompose(int root)
-{
-	for(vector <int> :: iterator it = g[centroid].begin(); it != g[centroid].end(); it++)
-		{
-				
-		}
-}
+	// 				lazy[level] = 0;
+	// 			}
 
-int main()
+	// 		if(start > b || end < a)
+	// 			return 0;
+	// 		else if(start >= a && end <= b)
+	// 			return val;
+	// 		else
+	// 			return merge(l -> query(lazy, start, mid, a, b, 2 * level), r -> query(lazy, mid + 1, end, a, b, 2 * level + 1));
+	// 	}
+};
+
+signed main()
 	{
-		int u, v, i, j, m;
+		ios_base::sync_with_stdio(false);
+		cin.tie(NULL);
+		cout.tie(NULL);
+		
+		// ifstream cin ("/Users/Aulene/Desktop/input.txt");
+		// ofstream cout ("/Users/Aulene/Desktop/output.txt");
+
+		// ifstream cin ("input.txt");
+		// ofstream cout ("output.txt");
+		
+		int n, m, i, j, u, v;
 
 		cin >> n >> m;
 
-		for(i = 0; i < n - 1; i++)
-			{
-				cin >> u >> v;
-				g[u].pb(v);
-				g[v].pb(u);
-			}
+		for(i = 0; i < n - 1; i++) {
+			cin >> u >> v;
+			g[u].pb(v), g[v].pb(u);
+		}
 
-		findCentroid(1);
+		dfs(1);
 
-		for(i = 1; i <= n; i++)
-			cout << subtrees[i] << " ";
-		cout << endl << centroid << endl;
+		cout << "DP[]: "; for(i = 1; i <= n; i++) cout << d[i] << " "; cout << endl;
+		cout << "T1[]: "; for(i = 1; i <= n; i++) cout << tin[i] << " "; cout << endl;
+		cout << "T2[]: "; for(i = 1; i <= n; i++) cout << tout[i] << " "; cout << endl;
 
-		for(i = 1; i <= n; i++)
-			vis[i] = 0;
+		RangeSTreeLazy *root = new RangeSTreeLazy;
+		root = root -> build(1, t);
 
-		decompose(centroid);
+		// while(m--) {
+		// 	cin >> u >> v;
 
-		for(i = 1; i <= n; i++)
-			cout << decompositions[i] << " ";
-		cout << endl;
+		// 	if(u == 1) {
+
+		// 	}
+		// 	else {
+
+		// 	}
+		// }
+
 		return 0;
 	}
